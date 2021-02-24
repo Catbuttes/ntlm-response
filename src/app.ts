679e0b77aa4b6f0131603ec3dd9e489974246853
CatButtes <catbuttes@outlook.com>
2021-02-24 20:19:53 +0000

    Add configurable config

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,45 @@ interface config {
     http_header_tags?: { [header: string]: string };
 }
 
-let configFile = 'config.json';
+if (process.argv.length != 3) {
+    console.log(
+        `NTLM-Response
+Usage: ./ntlm-response <config file>
+
+Returns an InfluxDB line protocol formatted set of metrics for targets defined in the config file
+
+Sample Config:
+
+{
+    "urls": [
+        "https://www.google.com"
+    ],
+    "http_proxy": "",
+    "response_timeout": 5000,
+    "method": "get",
+    "username": "domain\\username",
+    "password": "password",
+    "body": "",
+    "response_body_field": "",
+    "response_body_max_size": "",
+    "response_string_match": "",
+    "response_status_code": 0,
+    "headers": {
+        "Host": "githubcom"
+    },
+    "http_header_tags": {
+        "HTTP_HEADER": "TAG_NAME"
+    }
+}
+
+Source: https://github.com/Catbuttes/ntlm-response
+`
+    );
+
+process.exit(1);
+}
+
+let configFile = process.argv[2].toString();
 let configString = readFileSync(configFile);
 let config: config = JSON.parse(configString.toString());
 
@@ -84,7 +122,7 @@ async function getRequest(client: AxiosInstance, request: AxiosRequestConfig) {
             for (let header in config.http_header_tags) {
                 if (Object.prototype.hasOwnProperty.call(config.http_header_tags, header)) {
                     let tag = config.http_header_tags[header];
-                    if(response.headers[header] !== undefined) {
+                    if (response.headers[header] !== undefined) {
                         metric = metric + "," + tag + "=" + response.headers[header].toString();
                     }
                 }
@@ -147,30 +185,29 @@ function getResult(response: AxiosResponse): string {
 }
 
 function matchResponseString(response: AxiosResponse): string {
-    if(config.response_string_match === undefined)
-    {
+    if (config.response_string_match === undefined) {
         return "1"
     }
 
-    if(config.response_string_match === "") {
+    if (config.response_string_match === "") {
         return "1"
     }
 
-    if(response === undefined) {
+    if (response === undefined) {
         return "0"
     }
 
-    if(response.data === undefined) {
+    if (response.data === undefined) {
         return "0"
     }
 
-    if(response.data.length === 0) {
+    if (response.data.length === 0) {
         return "0"
     }
 
     let regex = new RegExp(config.response_string_match!)
 
-    if(regex.test(response.data)) {
+    if (regex.test(response.data)) {
         return "1"
     }
 
@@ -186,7 +223,7 @@ function matchResponseCode(response: AxiosResponse): string {
         return "1";
     }
     if (response === undefined) {
-        if(config.response_status_code === 408) {
+        if (config.response_status_code === 408) {
             return "1";
         }
 
